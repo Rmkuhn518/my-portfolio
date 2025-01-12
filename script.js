2c9808915a65dee8368320fbb1dd103dd8fb744b
Bub_Hub <117693250+Rmkuhn518@users.noreply.github.com>
2025-01-11 19:27:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('contactForm');
-    const hamburger = document.getElementById("hamburger");
-    const navMenu = document.getElementById("nav-menu");
-    
-    hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("show");
     });
 });
 
